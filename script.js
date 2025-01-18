2d7194ebad470db6f59460cb09b9b8677f416e12
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-18 21:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function makeid(l) {
   // write your code here
-}
+	let char_code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = "";
+
+	for(let i=0; i<l; i++) {
+		let char =  char_code.charAt(Math.floor(
+			Math.random()*char_code.length)
+		)
+		result+=char;
+	}
+	return result;
+}   
+ 
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
